9022ed76dce8363b86fb1652dfe4b3d65632e1cd
MathSoares88 <matheus.ssoares@sptech.school>
2024-06-12 16:24:42 -0300

    Rota calculadora

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,24 +2,31 @@ const express = require('express');
 const router = express.Router();
 
 // Rota raiz dentro do grupo /
-router.get('/', function(req, res) {
+router.get('/', function (req, res) {
     let errorMessage = '';
     let hasError = false;
 
-    if(req.session.hasError){
+    if (req.session.hasError) {
         errorMessage = req.session.errorMessage;
         hasError = req.session.hasError;
 
         delete req.session.hasError;
         delete req.session.errorMessage;
-        
+
         console.log(errorMessage);
     }
 
-    res.render('index', { 
+    res.render('index', {
         hasError: hasError,
         errorMessage: errorMessage
-        });
+    });
+});
+
+router.get('/calculadora', function (req, res) {
+    let errorMessage = '';
+    let hasError = false;
+
+    res.render('calculadora.ejs');  
 });
 
 module.exports = router;
